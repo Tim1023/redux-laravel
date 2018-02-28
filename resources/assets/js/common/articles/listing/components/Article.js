d7f9c4133cda78dc92f42238ccc354aa84c30973
Tim <timzdeveloper23@gmail.com>
2018-02-28 18:50:42 +1300

    Update articles with material design

--- resources/assets/js/common/articles/listing/components/Article.js
+++ resources/assets/js/common/articles/listing/components/Article.js
@@ -1,35 +1,34 @@
 // import libs
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import Paper from 'material-ui/Paper';
 // import components
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import Typography from 'material-ui/Typography';
 
 const displayName = 'ArticleComponent'
 const propTypes = {
-  index: PropTypes.number.isRequired,
-  article: PropTypes.object.isRequired,
+    index: PropTypes.number.isRequired,
+    article: PropTypes.object.isRequired,
 }
 
-// const renderAuthor = (article) => {
-//   return article.user && `By ${ article.user.name }`
-// }
+const renderAuthor = (article) => {
+  return article.user && `By ${ article.user.name }`
+}
 
 const renderPublishedAt = (article) => {
-  return article.publishedAt && `at ${article.publishedAt.format('MMMM D, YYYY')}`
+    return article.publishedAt && `at ${article.publishedAt.format('MMMM D, YYYY')}`
 }
 
-function render ({ article }) {
-  return <div className="col-12 col-sm-9 mb-5 mx-auto">
-    <div className="card">
-      <div className="card-body">
-        <h4 className="card-title">{article.title}</h4>
-        <h6 className="card-subtitle mb-2 text-muted">{renderPublishedAt(article)}</h6>
-        <p className="card-text">{ article.description }</p>
-        <Link to={`blog/${article.slug}`} className="card-link">Read More</Link>
-      </div>
-    </div>
-  </div>
+function render({article}) {
+    return <Paper className="col-12 col-sm-9 mb-5 mx-auto" elevation={6}>
+        <div className="card-body">
+            <Typography variant="headline" component="h3">{article.title}</Typography>
+            <Typography className="card-subtitle mb-2 text-muted">{renderPublishedAt(article)} {renderAuthor(article)}</Typography>
+            <Typography component="p">{article.description}</Typography>
+            <Link to={`blog/${article.slug}`} className="card-link">Read More</Link>
+        </div>
+    </Paper>
 }
 
 render.displayName = displayName
