f854aa731df09874be97f61c33a60951d7d00c40
Tim <timzdeveloper23@gmail.com>
2018-03-04 10:47:58 +1300

    Update scrollTop

--- resources/assets/js/common/scroll-top/ScrollTop.js
+++ resources/assets/js/common/scroll-top/ScrollTop.js
@@ -1,70 +1,81 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import Button from 'material-ui/Button';
+import ArrowUpward from 'material-ui-icons/ArrowUpward';
+import blue from 'material-ui/colors/blue';
 
 const style = {
-  float: 'right',
-  position: 'fixed',
-  bottom: '1rem',
-  right: '1rem',
+    link: {
+        float: 'right',
+        position: 'fixed',
+        bottom: '1rem',
+        right: '1rem',
+    },
+    button: {
+        backgroundColor: blue["500"],
+    }
+
 }
 
 class ScrollTop extends Component {
-  constructor() {
-    super()
-    
-    this.delayInMs = '16'
-    this.scrollStepInPx = 50
-    
-    this.state = {
-      intervalId: 0,
-      showScoller: false,
+    constructor() {
+        super()
+
+        this.delayInMs = '16'
+        this.scrollStepInPx = 50
+
+        this.state = {
+            intervalId: 0,
+            showScoller: false,
+        }
+
+        this.toggleScroll = this.toggleScroll.bind(this)
+        this.scrollStep = this.scrollStep.bind(this)
     }
-    
-    this.toggleScroll = this.toggleScroll.bind(this)
-    this.scrollStep = this.scrollStep.bind(this)
-  }
-  
-  componentDidMount() {
-    window.addEventListener("scroll", this.toggleScroll)
-  }
-  
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.toggleScroll)
-  }
-  
-  toggleScroll() {
-    if (window.pageYOffset > 200) {
-      this.setState({ showScoller: true })
-    } else {
-      this.setState({ showScoller: false })
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.toggleScroll)
     }
-  }
-  
-  scrollStep() {
-    if (window.pageYOffset === 0) {
-      clearInterval(this.state.intervalId)
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.toggleScroll)
     }
-    window.scroll(0, window.pageYOffset - this.scrollStepInPx)
-  }
-  
-  scrollToTop(e) {
-    e.preventDefault()
-    
-    let intervalId = setInterval(this.scrollStep, this.delayInMs)
-    this.setState({ intervalId: intervalId })
-  }
-  
-  render() {
-    if (this.state.showScoller) {
-      return (<a href="#" title='Back to top'
-                 style={style}
-                 onClick={(e) => { this.scrollToTop(e) }}>
-        <i className="fa fa-chevron-circle-up"
-           style={{ fontSize: '3rem' }}
-           aria-hidden="true" />
-      </a>)
+
+    toggleScroll() {
+        if (window.pageYOffset > 200) {
+            this.setState({showScoller: true})
+        } else {
+            this.setState({showScoller: false})
+        }
     }
-    return null
-  }
+
+    scrollStep() {
+        if (window.pageYOffset === 0) {
+            clearInterval(this.state.intervalId)
+        }
+        window.scroll(0, window.pageYOffset - this.scrollStepInPx)
+    }
+
+    scrollToTop(e) {
+        e.preventDefault()
+
+        let intervalId = setInterval(this.scrollStep, this.delayInMs)
+        this.setState({intervalId: intervalId})
+    }
+
+    render() {
+        if (this.state.showScoller) {
+            return (<a href="#" title='Back to top'
+                       style={style.link}
+                       onClick={(e) => {
+                           this.scrollToTop(e)
+                       }}>
+                <Button variant="fab" className="text-white" style={style.button}>
+                    <ArrowUpward/>
+                </Button>
+            </a>)
+        }
+        return null
+    }
 }
 
 export default ScrollTop
